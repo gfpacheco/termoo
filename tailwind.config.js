ab38e69a992947c5e92d65ab70950ac4e73eaeb8
Guilherme Pacheco <guilherme.f.pacheco@gmail.com>
2022-02-07 11:31:56 -0300

    Add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ['./pages/**/*.tsx', './components/**/*.tsx'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
